c5f575a94900941b4aab5f4780a8880e0811cb35
IvanXdelta <ivmrsic@fesb.hr>
2024-08-03 17:40:06 +0200

    minor database table fixes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('company');
-            $table->string('tax_number');
+            $table->string('company')->nullable();
+            $table->string('tax_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-
         });
     }
 
--- database/migrations/2024_07_25_224049_create_contract_lists_table.php
+++ database/migrations/2024_07_25_224049_create_contract_lists_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('contract', function (Blueprint $table) {
+        Schema::create('contract_lists', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->reference('id')->on('users');
             $table->double('price', 9, 2);
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('contract');
+        Schema::dropIfExists('contract_lists');
     }
 };
