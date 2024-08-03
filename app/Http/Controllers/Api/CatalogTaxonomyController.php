6cb9e35f1009d882cba6d6c07fbcdd5f69a0b82f
IvanXdelta <ivmrsic@fesb.hr>
2024-08-03 17:47:39 +0200

    Renamed taxonomy and category to catalog taxonomy and catalog category

--- app/Http/Controllers/Api/CatalogTaxonomyController.php
+++ app/Http/Controllers/Api/CatalogTaxonomyController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TaxonomyController extends Controller
+class CatalogTaxonomyController extends Controller
 {
     //
 }
--- app/Models/CatalogCategory.php
+++ app/Models/CatalogCategory.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Taxonomy extends Model
+class CatalogCategory extends Model
 {
     use HasFactory;
 }
